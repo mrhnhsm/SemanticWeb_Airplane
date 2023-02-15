c3942440d7e7c474ba54d3cf2ab59e0032876688
mrhnhsm <rayhanhasyim00@gmail.com>
2023-02-15 07:39:19 +0700

    SemanticWeb_Airplane

---
+++ Source Code/index.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> AIRPLANE </title>
+        <link rel="stylesheet"href="css/bootstrap.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <!-- Favicons -->
+  <link href="logopc.jpg" rel="icon">
+  <link href="logopc.jpg" rel="apple-touch-icon">
+        
+ </head>
+    <body> 
+    
+        <!--awal navbar-->
+        
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <img src="logopc.jpg" height="34 px">
+        
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h3>AIRPLANE</h3></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="#">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#schedule">Skejul</a>
+        </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#plane">Pesawat</a>
+        </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#location">Lokasi Kantor</a>
+        </li>
+          <li class="nav-item">
+          <a class="nav-link" href="#assistance">Assistance</a>
+          </li>
+          
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"></a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+        
+        <!--akhir navbar-->
+        
+        <!--awal home-->
+        
+        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="BG1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Ingin Tahu Jenis Pesawat Di Dunia?</h5>
+        <p>Cari Tahu Aja Di Airplane</p>
+      </div>
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="BG2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Ingin Tahu Maskapai Di Dunia?</h5>
+        <p>Semua Ada Di Airplane</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="bg3.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Pilihan Maskapai Terbaik?</h5>
+        <p>Ada Di Airplane</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+        
+        <!--akhir home-->
+        
+        <!--Awal Schedule-->
+        
+        <br>
+        <br>
+        <br>
+        
+        
+        <div class="jumbotron" id="schedule">
+        <div class="container">
+          <h1 class="display-4">Skejul Pesawat!</h1>
+            
+             <table class="table table-striped"> 
+          <thead> 
+              <th>Waktu</th>
+              <th>Senin</th>
+              <th>Selasa</th>
+              <th>Rabu</th>
+              <th>Kamis</th>
+              <th>Jumat</th>
+          </thead>
+        <tr>
+            <th>08.00 - 09.00</th>
+            <td>Garuda Indonesia</td>
+            <td>Oman Air</td>
+            <td>Japan Airlines</td>
+            <td>Finnair</td>
+            <td>Sriwijaya Air</td> 
+        </tr>
+        <tr> 
+            <th>09.10 - 10.10</th>
+            <td>Qantas Airways</td>
+            <td>Austrian Airlines</td>
+            <td>Turkish Airlines</td>
+            <td>Philippine Airlines</td>
+            <td>Wings Air</td>
+        </tr>
+        <tr>
+            <th>10.20 - 11.20</th>
+            <td>Bangkok Airways</td>
+            <td>Citilink</td>
+            <td>Lion Air</td>
+            <td>Batik Air</td>
+            <td>Batik Air</td>
+        </tr>
+        <tr>
+            <th>11.30 - 13.30</th>
+            <td>Batik Air</td>
+            <td>Batik Air</td>
+            <td>Etihad Airways</td>
+            <td>EVA Air</td>
+            <td>Hainan Airlines</td>
+        </tr>
+        <tr>
+            <th>13.40 - 14.40</th>
+            <td>Super Air Jet</td>
+            <td>Emirates/KLM Royal Dutch Airlines</td>
+            <td>Virgin Australia</td>
+            <td>Lufthansa</td>
+            <td>Air France</td>
+        </tr>
+        <tr>
+            <th>13.40 - 14.40</th>
+            <td>Swiss International Air Lines</td>
+            <td>China Southern Airlines</td>
+            <td>Singapore Airlines/Hong Kong Airlines</td>
+            <td>Cathay DragonCathay Dragon</td>
+            <td>Korean Air</td>
+        </tr>
+        
+        </table>
+          
+        </div>
+        </div>
+        
+        <!--Akhir Schedule-->
+
+        
+        <!--Awal Card-->
+        
+        <br>
+        <br>
+        <br>
+            
+           <div class="container-fluid mt-5" id="plane"> 
+           <div class="row"> 
+               
+                <!-- Awal Query Pesawat-->
+            <?php
+                require_once("sparqllib.php");
+                
+                $db = sparql_connect("http://localhost:3030/plane/query");
+                if( !$db ) {print sparql_errno() . ": " . sparql_error(). "\n"; exit;}
+            
+                $sparql = " PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX ar: <http://www.tugasakhirlabsw/namespace#>
+
+                            SELECT ?link ?maskapai ?description ?gambar
+                            WHERE{ 
+                            ?subject ar:link ?link .
+                            ?subject ar:maskapai ?maskapai . 
+                            ?subject ar:description ?description .
+                            ?subject ar:gambar ?gambar 
+                            }";
+            
+                $result = sparql_query($sparql);
+                $fields = sparql_field_array($result);
+            
+                while( $row = sparql_fetch_array($result))
+                {
+                    print "<div class='col-lg-3 col-md-6'>";
+                    print "<div class='card'>";
+                    print "<img src='$row[gambar]' class='card-img-top' alt='...'>";
+                    print"<div class='card-body'>";
+                    print"<h5 class='card-title'>$row[maskapai]</h5>";
+                    print"<p class='card-text'>$row[description]</p>";
+                    print"<a href='$row[link]' class='btn btn-primary'>Jelajahi Maskapai</a>";
+                    
+                    print"</div>";
+                    print"</div>";
+                    print"</div> ";
+                    
+                   
+                }
+            ?>
+            <!-- Akhir Query Pesawat-->
+               
+</div>
+    </div>
+
+        <!--Akhir Card-->
+        
+      
+         <!--Awal Peta-->
+        
+        <div class="embed-responsive embed-responsive-16by9 mt-5" id="location">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d995.7834627447295!2d99.18747588805361!3d3.317074238706486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x303161dfc6f3fe91%3A0x6521c2b5892743b6!2sTebing%20Tinggi%2C%20Kec.%20Padang%20Hilir%2C%20Kota%20Tebing%20Tinggi%2C%20Sumatera%20Utara%2020998!5e0!3m2!1sid!2sid!4v1639411544588!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>"
+        </div>
+        
+        <!--Akhir Peta-->
+        
+          <!--Awal Footer-->
+        <br>
+        <br>
+        <br>
+        <br>
+        
+  <footer class="footer mt-5 mb-5 text-center" id="assistance">
+     <img src="logopc.jpg" height="66px" class="mr-3">  AIRPLANE <hr> M. Raihan Efdin Hasyim &copy;2020
+        </footer>
+
+        
+        <!--Akhir Footer-->
+       
+        <!-- Awal Script Js-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        <!-- Akhir SCRIPT Js-->
+                  
+     </body>                
+    
+</html>
